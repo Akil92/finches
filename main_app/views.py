9e8ebb73e584df5be7b47f97ec5c31066012d26e
Akil92 <akilmaynard@yahoo.com>
2023-08-07 20:04:23 -0400

    second

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
+from .models import Finch
 
-finches = [
-    {'name': 'Tweety', 'description': 'yellow', 'age': 6},
-    {'name': 'Tucan', 'description': 'rainbow', 'age': 4}
-]
 
 # Create your views here.
 def about(request):
     return render(request, 'about.html')
 
-def finch_index(request):
+def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finch/index.html', { 'finches': finches})   
 
 def home(request):
     return render(request, 'home.html')
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finch/details.html', {'finch': finch})
